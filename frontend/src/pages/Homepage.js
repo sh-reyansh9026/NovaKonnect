3db67acbba270569ee4f2151417c2952fcb9566f
sh-reyansh9026 <sh.reyansh9026@gmail.com>
2024-09-06 00:43:19 +0530

    Regular Development Update 2
    Implemented Login/Signup page frontend

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,11 +1,54 @@
 import React from 'react'
-
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 const Homepage = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
+    <Container maxW='xl' centerContent>
+      {/* <Box>
+        <Text>NovaKonnect</Text>
+      </Box> */}
+      <Box
+        d='flex'
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black" textAlign="center">
+          NovaKonnect
+        </Text>
+      </Box>
+            <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 }
 
-export default Homepage
+export default Homepage;
