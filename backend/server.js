40753999621117036abf1f3ea9d6d6deb7c94959
sh-reyansh9026 <sh.reyansh9026@gmail.com>
2024-09-06 01:30:22 +0530

    Regular Development Update 3
    Connection of MongoDB(Atlas) with node js server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
-
-const app = express()
+const connectDB = require("./config/db");
+const colors = require("colors");
 dotenv.config();
+connectDB();
+const app = express();
 
 app.get('/', (req, res) => {
     res.send("API is running successfully");
@@ -19,5 +21,5 @@ app.get("/api/chat/:id", (req, res) => {
     res.send(singleChat);
 });
 const PORT = process.env.PORT || 5000;
-app.listen(5000, console.log(`Server started on PORT ${PORT}`));
+app.listen(5000, console.log(`Server started on PORT ${PORT}`.yellow.bold));
  
