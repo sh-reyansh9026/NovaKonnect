6873ce0d6269cf6103b0edbff5945aaacc4146fe
sh-reyansh9026 <sh.reyansh9026@gmail.com>
2024-09-07 01:30:29 +0530

    Regular Development Update 4
    Login/Signup backend integration using Registration API + Encryption of password using bcryptjs +
    Integration of Cloudinary service for image storage +Login/Signup frontend, backend integration + Guest login functionality.

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -7,20 +7,136 @@ import axios from "axios";
 import { useState } from "react";
 import { useHistory } from "react-router";
 
-
 const Signup = () => {
   const [show, setShow] = useState(false);
+  const toast = useToast();
+  const history = useHistory();
+
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [confirmpassword, setConfirmpassword] = useState();
   const [password, setPassword] = useState();
   const [pic, setPic] = useState();
+  const [picLoading, setPicLoading] = useState(false);
+
   const handleClick = () => setShow(!show);
-  const postDetails = (pics) => { };
-  const submitHandler = () => {};
-    return (
-      
-     <VStack spacing="5px">
+
+  const postDetails = (pics) => {
+    setPicLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "NovaKonnect");
+      data.append("cloud_name", "dc4kwbueo");
+
+       axios
+         .post("https://api.cloudinary.com/v1_1/dc4kwbueo/image/upload", data)
+         .then((response) => {
+           console.log("Cloudinary response:", response);
+           setPic(response.data.url.toString());
+           setPicLoading(false);
+           toast({
+             title: "Image uploaded successfully!",
+             status: "success",
+             duration: 5000,
+             isClosable: true,
+             position: "bottom",
+           });
+         })
+         .catch((err) => {
+           console.log(err);
+           setPicLoading(false);
+         });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+  };
+
+  const submitHandler = async () => {
+    setPicLoading(true);
+    if (!name || !email || !password || !confirmpassword) {
+      toast({
+        title: "Please Fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+    if (password !== confirmpassword) {
+      toast({
+        title: "Passwords Do Not Match",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    console.log(name, email, password, pic);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user",
+        {
+          name,
+          email,
+          password,
+          pic,
+        },
+        config
+      );
+      console.log(data);
+      toast({
+        title: "Registration Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setPicLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+    }
+  };
+
+  return (
+    <VStack spacing="5px">
       <FormControl id="first-name" isRequired>
         <FormLabel>Name</FormLabel>
         <Input
@@ -80,6 +196,7 @@ const Signup = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={picLoading}
       >
         Sign Up
       </Button>
